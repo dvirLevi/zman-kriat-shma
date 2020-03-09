67437216dadf2885b980d6154610133d4d0e9353
dvirLevi <dvirleviapp@gmail.com>
2020-03-10 01:08:42 +0200

    add

---
+++ dist/precache-manifest.07579f98a3bd6d9c33bc7f0c831003d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b97c552660dcfdb8156db7fe7083b11",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "f32977c037af2843b168",
+    "url": "/css/app.13ea21e5.css"
+  },
+  {
+    "revision": "ee3dc73d6cedd0b020f1",
+    "url": "/css/chunk-6a1eba3c.ef966cfc.css"
+  },
+  {
+    "revision": "88efa761cf679a3d662e1e855b793f0e",
+    "url": "/img/ass1.88efa761.png"
+  },
+  {
+    "revision": "cfbad45a42cffbc6e4a7881655e7d027",
+    "url": "/img/logo.cfbad45a.png"
+  },
+  {
+    "revision": "8c2504485147a4b2763b50243552b4cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f32977c037af2843b168",
+    "url": "/js/app.4555f1c4.js"
+  },
+  {
+    "revision": "ee3dc73d6cedd0b020f1",
+    "url": "/js/chunk-6a1eba3c.968f5757.js"
+  },
+  {
+    "revision": "d41e436811aeabac8b1a",
+    "url": "/js/chunk-vendors.2396618c.js"
+  },
+  {
+    "revision": "9078eef549c416f4aa3438e1a1797746",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
