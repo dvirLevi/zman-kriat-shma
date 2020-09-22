3dd878d87c738e0644f30c1cd61802ebb271db9b
dvirLevi <dvirleviapp@gmail.com>
2020-09-22 11:53:00 +0300

    add

--- dist/precache-manifest.9098473d8d6cc0d2e4452a447c905c36.js
+++ dist/precache-manifest.9098473d8d6cc0d2e4452a447c905c36.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "887a4ffaca3a7143eddf",
-    "url": "/css/app.4a91c829.css"
+    "revision": "a1d3e31cccbc74913680",
+    "url": "/css/app.ed970ba7.css"
   },
   {
     "revision": "ba9594f84252db9e4759",
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/img/logo.8ad12d7d.png"
   },
   {
-    "revision": "96c853383c81ed4e8ec73f58dfb36190",
+    "revision": "a0b3ef5cbe1247f94a650363f98d85f5",
     "url": "/index.html"
   },
   {
-    "revision": "887a4ffaca3a7143eddf",
-    "url": "/js/app.53314481.js"
+    "revision": "a1d3e31cccbc74913680",
+    "url": "/js/app.f26597e1.js"
   },
   {
     "revision": "ba9594f84252db9e4759",
